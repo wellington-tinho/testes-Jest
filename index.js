2b60038d995b92d2483b0523243b2a9d460102ea
wrtinho <tinho1068@hotmail.com>
2020-07-28 13:50:52 -0300

    usando prompt-sync

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const calculadora = require("./calc")
 
-const result = calculadora.divisao(20,2)
+const prompt = require('prompt-sync')()
 
-console.log(result);
+function optionSelected(option) {
+    
+    const num1 = Number(prompt("Digite o numero 1: "))
+    const num2 = Number(prompt("Digite o numero 2: "))
+    
+    if(option == "1"){
+        return calculadora.soma(num1,num2)
+    }
+    if(option == "2"){
+        return calculadora.subtracao(num1,num2)
+    }
+}
+
+
+function options() {
+    console.log(`
+    1 - somar
+    2 - subtrair 
+    3 - multiplicar 
+    4 - dividir
+    0 - sair
+    `)
+}
+let option;
+while (option != '0') {
+    options();
+    option = prompt("Qual a opção? ");
+
+    if(option != '0'){
+        const resultado = optionSelected(option);
+        console.log(`O resltado da operação foi ${resultado}`);
+    }else{
+        console.log("Voce escolheu sair");
+    }
+}
+
+
